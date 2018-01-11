7a1268ce288e9a14c9c23ca72c2a31021beef10f
mathivhahy <mrmathivhahy@gmail.com>
2018-01-11 12:16:24 +0200

    Done reorganizing code

---
+++ firebase/public/js/petprofile-page.js
@@ -0,0 +1,9 @@
+/* front-end dynamism */
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
